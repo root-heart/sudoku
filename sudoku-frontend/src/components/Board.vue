5b66f82196d964402921632d6cb95a00f5009464
kai <TurboQP10_github>
2020-05-12 21:45:58 +0200

    add basic sudoku board component

---
+++ sudoku-frontend/src/components/Board.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="board">
+        <Block></Block>
+        <Block></Block>
+        <Block></Block>
+        <Block></Block>
+        <Block></Block>
+        <Block></Block>
+        <Block></Block>
+        <Block></Block>
+        <Block></Block>
+    </div>
+</template>
+
+<script>
+    import Block from "./Block";
+
+    export default {
+        name: "Board",
+        components: {Block}
+    }
+</script>
+
+<style scoped>
+.board {
+    display: grid;
+    grid-template-columns: auto auto auto;
+    grid-template-rows: auto auto auto;
+}
+</style>
