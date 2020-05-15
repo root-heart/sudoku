5be6db7db2b67c5cec7fc8353d1011274a577dd0
kai <TurboQP10_github>
2020-05-15 21:35:26 +0200

    implement a basic board reacting to user input

--- sudoku-frontend/src/App.vue
+++ sudoku-frontend/src/App.vue
@@ -1,27 +1,30 @@
 <template>
-  <div id="app">
-    <Board></Board>
-  </div>
+    <div id="app">
+        <Board></Board>
+    </div>
 </template>
 
 <script>
-import Board from "./components/Board";
+    import Board from "./components/Board";
 
-export default {
-  name: 'App',
-  components: {
-    Board
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            Board
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
+
+    body {
+        margin: 0;
+    }
 </style>
--- sudoku-frontend/src/components/Board.vue
+++ sudoku-frontend/src/components/Board.vue
@@ -1,30 +1,94 @@
 <template>
-    <div class="board">
-        <Block></Block>
-        <Block></Block>
-        <Block></Block>
-        <Block></Block>
-        <Block></Block>
-        <Block></Block>
-        <Block></Block>
-        <Block></Block>
-        <Block></Block>
-    </div>
+    <form @submit.prevent="validateGame">
+        <table class="board"
+               @keypress="enterDigit"
+               tabindex="-1">
+            <tr v-for="row in 9" :class="'row' + row" :key="row">
+                <td v-for="column in 9"
+                    :key="column"
+                    :class="['column' + column,
+                                selected.row === row && selected.column === column ? 'selected' : '']"
+                    @click="selectField(column, row)">
+                    <span v-if="board[row * 9 + column - 10] > 0">
+                        {{board[row * 9 + column - 10]}}
+                    </span>
+                </td>
+            </tr>
+        </table>
+
+        <div class="gameActions">
+            <button @click="newGame">New Game</button>
+            <button @click="validateGame">Validate Game</button>
+        </div>
+    </form>
 </template>
 
 <script>
-    import Block from "./Block";
-
     export default {
         name: "Board",
-        components: {Block}
+        data() {
+            let board = [];
+            for (let i = 0; i < 81; i++) {
+                board.push(0);
+            }
+            board[21] = 3;
+            return {
+                board: board,
+                selected: {row: 0, column: 0}
+            }
+        },
+        methods: {
+            newGame() {
+
+            },
+            validateGame() {
+
+            },
+            selectField(column, row) {
+                this.selected.column = column;
+                this.selected.row = row;
+            },
+            enterDigit(event) {
+                if (event.key >= 1 && event.key <= 9) {
+                    console.log(event);
+                    this.board[this.selected.column + this.selected.row * 9 - 10] = event.key;
+                    this.$forceUpdate();
+                }
+            }
+        }
     }
 </script>
 
 <style scoped>
-.board {
-    display: grid;
-    grid-template-columns: auto auto auto;
-    grid-template-rows: auto auto auto;
-}
+    .board {
+        border: 3px solid #333;
+        border-collapse: collapse;
+        table-layout: fixed;
+        width: calc(100vh / 11 * 9);
+        font-size: calc(100vh / 11);
+    }
+
+    .board td {
+        border: 1px solid #333;
+        width: calc(100vh / 11);
+        height: calc(100vh / 11);
+    }
+
+    .board .row3, .board .row6 {
+        border-bottom: 2px solid #333;
+    }
+
+    .board .column3, .board .column6 {
+        border-right: 2px solid #333;
+    }
+
+    .selected {
+        background-color: beige;
+    }
+
+    form {
+        display: grid;
+        grid-template-columns: 70% 30%;
+        box-sizing: border-box;
+    }
 </style>
