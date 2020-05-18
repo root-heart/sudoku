be6d738437518f7a0b6132bfc685621dcd8869b3
kai <TurboQP10_github>
2020-05-18 18:11:45 +0200

    remove unnecessary method

--- sudoku-backend/src/main/java/rootheart/codes/sudoku/game/CellList.java
+++ sudoku-backend/src/main/java/rootheart/codes/sudoku/game/CellList.java
@@ -2,7 +2,6 @@ package rootheart.codes.sudoku.game;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
 
@@ -30,12 +29,6 @@ class CellList {
 
     public void forEach(Consumer<Cell> consumer) {
         cells.forEach(consumer);
-    }
-
-    public void forEachWithIndex(BiConsumer<Integer, Cell> consumer) {
-        for (int index = 0; index < cells.size(); index++) {
-            consumer.accept(index++, cells.get(index));
-        }
     }
 
     public Cell getCell(int index) {
