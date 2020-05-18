105e143c427568aa2260e9a92d6aec5cf05b8715
kai <TurboQP10_github>
2020-05-18 17:54:34 +0200

    improve readability of recursive brute force implementation

--- sudoku-backend/src/main/java/rootheart/codes/sudoku/solver/Solver.java
+++ sudoku-backend/src/main/java/rootheart/codes/sudoku/solver/Solver.java
@@ -8,60 +8,59 @@ import java.util.List;
 public class Solver {
 
     public void solve(Board board) {
-        while (board.hasEmptyCells()) {
+        if (!board.hasEmptyCells()) {
+            return;
+        }
+        if (!board.isValid()) {
+            throw new BoardInvalidException();
+        }
+        SolverBoard solverBoard = new SolverBoard(board);
+        solverBoard.calculate();
+        if (!solverBoard.hasSolution()) {
+            throw new NoSolutionException("found no solution");
+        }
+
+        int singleCandidateCount = solverBoard.getSingleCandidates().size();
+        solverBoard.getSingleCandidates().forEach(solverCell -> {
+            solverCell.getCell().setNumber(solverCell.getFirstCandidate());
             if (!board.isValid()) {
                 throw new BoardInvalidException();
             }
-            SolverBoard solverBoard = new SolverBoard(board);
-            solverBoard.calculate();
-            if (!solverBoard.hasSolution()) {
-                throw new NoSolutionException("found no solution");
-            }
-//            Board previousState = clone(board);
-
-            if (solverBoard.getSingleCandidates().size() == 0) {
-//                System.out.println("XXX");
-                Board boardToSetARandomNumberTo = clone(board);
-                List<Board> solutions = new ArrayList<>();
-                boardToSetARandomNumberTo.streamEmptyCells()
-                        .findFirst()
-                        .ifPresent(cell ->
-                                boardToSetARandomNumberTo.getPossibleValues().forEach(numberToTry -> {
-                                    cell.setNumber(numberToTry);
-                                    if (boardToSetARandomNumberTo.isValid()) {
-//                                        if (System.currentTimeMillis() % 500 == 0) {
-//                                            System.out.println(previousState.getBoardString());
-//                                            int index = boardToSetARandomNumberTo.indexOf(cell);
-//                                            System.out.println(" ".repeat(Math.max(0, index)) + numberToTry);
-//                                        }
-                                        Board boardToTryToSolve = clone(boardToSetARandomNumberTo);
-                                        try {
-                                            solve(boardToTryToSolve);
-                                            solutions.add(boardToTryToSolve);
-                                        } catch (NoSolutionException e) {
-                                            // if trying this number did not end up with a solution, try the next one
-                                        }
-                                    }
-                                }));
-                if (solutions.size() == 0) {
-                    throw new NoSolutionException("found no solution (2)");
-                }
-                if (solutions.size() > 1) {
-                    throw new MultipleSolutionsException("found multiple solutions");
-                }
-                board.set(solutions.get(0).getBoardString());
-            } else {
-                solverBoard.getSingleCandidates().forEach(solverCell -> {
-                    solverCell.getCell().setNumber(solverCell.getFirstCandidate());
-                    if (!board.isValid()) {
-                        throw new BoardInvalidException();
-                    }
-                });
-//                System.out.println(previousState.getBoardString());
-//                int index = board.indexOf(next.getKey());
-//                System.out.println(" ".repeat(Math.max(0, index)) + next.getValue());
-            }
+        });
+        long emptyCellCount = board.streamEmptyCells().count();
+        System.out.println("single candidates: " + singleCandidateCount + "  remaining empty cells: " + emptyCellCount);
+        if (emptyCellCount > 0) {
+            solveBruteForce(board);
         }
+    }
+
+    private void solveBruteForce(Board board) {
+        Board boardToSetARandomNumberTo = clone(board);
+        List<Board> solutions = new ArrayList<>();
+        boardToSetARandomNumberTo.streamEmptyCells()
+                .findFirst()
+                .ifPresent(cell ->
+                        boardToSetARandomNumberTo.getPossibleValues().forEach(numberToTry -> {
+                            cell.setNumber(numberToTry);
+                            if (boardToSetARandomNumberTo.isValid()) {
+                                Board boardToTryToSolve = clone(boardToSetARandomNumberTo);
+                                try {
+                                    System.out.println("Try number " + numberToTry);
+                                    solve(boardToTryToSolve);
+                                    solutions.add(boardToTryToSolve);
+                                } catch (NoSolutionException e) {
+                                    // if trying this number did not end up with a solution, try the next one
+                                    System.out.println("No solution for " + numberToTry + ": " + e.getMessage());
+                                }
+                            }
+                        }));
+        if (solutions.size() == 0) {
+            throw new NoSolutionException("found no solution (2)");
+        }
+        if (solutions.size() > 1) {
+            throw new MultipleSolutionsException("found multiple solutions");
+        }
+        board.set(solutions.get(0).getBoardString());
     }
 
     private Board clone(Board board) {
