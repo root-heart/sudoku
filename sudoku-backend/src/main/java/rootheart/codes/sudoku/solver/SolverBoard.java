8286e834c6a6670006d3b4f182329c4fc8cb62a2
kai <TurboQP10_github>
2020-05-19 11:16:58 +0200

    simplify code

--- sudoku-backend/src/main/java/rootheart/codes/sudoku/solver/SolverBoard.java
+++ sudoku-backend/src/main/java/rootheart/codes/sudoku/solver/SolverBoard.java
@@ -13,11 +13,11 @@ import java.util.stream.Collectors;
 @Getter
 public class SolverBoard {
 
-    private final Map<Cell, SolverCell> solverCellMap;
     private final Set<SolverCell> singleCandidates = new HashSet<>();
+    private final Set<SolverCell> emptyCells;
 
     public SolverBoard(Board board) {
-        solverCellMap = board.streamCells()
+        Map<Cell, SolverCell> solverCellMap = board.streamCells()
                 .map(cell -> new SolverCell(cell, board))
                 .collect(Collectors.toMap(SolverCell::getCell, Function.identity()));
         solverCellMap.forEach((cell, solverCell) -> {
@@ -35,37 +35,27 @@ public class SolverBoard {
                     .forEach(c -> solverCell.getOtherCellsInBlock().add(c));
         });
         solverCellMap.values().forEach(SolverCell::initializationComplete);
+        emptyCells = solverCellMap.values().stream().filter(SolverCell::isEmpty).collect(Collectors.toSet());
     }
 
     public void eliminateImpossibleCandidates() {
-        long countBefore = countCellsWithSingleCandidate();
-        while (true) {
-            solverCellMap.values().forEach(SolverCell::eliminateImpossibleCandidates);
-            long countAfter = countCellsWithSingleCandidate();
+        for (long countBefore = singleCandidates.size(); ; ) {
+            for (SolverCell cell : emptyCells) {
+                cell.eliminateImpossibleCandidates();
+                if (cell.hasOneCandidate()) {
+                    singleCandidates.add(cell);
+                }
+            }
+            long countAfter = singleCandidates.size();
             if (countAfter == 0 || countBefore == countAfter) {
-                findNakedSingles();
                 return;
             }
             countBefore = countAfter;
         }
     }
 
-    private long countCellsWithSingleCandidate() {
-        return solverCellMap.values()
-                .stream()
-                .filter(SolverCell::hasOneCandidate)
-                .count();
-    }
-
     public boolean isNotSolvable() {
-        return solverCellMap.values().stream()
-                .anyMatch(entry -> entry.isEmpty() && entry.getCandidates().size() == 0);
-    }
-
-    private void findNakedSingles() {
-        solverCellMap.values()
-                .stream()
-                .filter(SolverCell::hasOneCandidate)
-                .forEach(singleCandidates::add);
+        return emptyCells.stream()
+                .anyMatch(entry -> entry.getCandidates().size() == 0);
     }
 }
