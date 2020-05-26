e4ff99dfb20c8b941d49322a78d9865e8cf26268
kai <TurboQP10_github>
2020-05-26 21:07:52 +0200

    introduce a call counter for some methods

--- sudoku-backend/src/main/java/rootheart/codes/sudoku/solver/binaryoptimized/Board.java
+++ sudoku-backend/src/main/java/rootheart/codes/sudoku/solver/binaryoptimized/Board.java
@@ -3,11 +3,12 @@ package rootheart.codes.sudoku.solver.binaryoptimized;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-class Board {
+public class Board {
     static final int[] BLOCK_INDEX = new int[81];
     static final int[] CELL_INDEX_IN_BLOCK = new int[81];
 
-    static final int[] indexMap = new int[81];
+    static final int[] columnIndexMap = new int[81];
+    static final int[] rowIndexMap = new int[81];
 
     static {
         for (int cellIndex, rowIndex = cellIndex = 0; rowIndex < 9; rowIndex++) {
@@ -22,24 +23,37 @@ class Board {
             int rowIndex = i / 9;
             int blockIndex = (rowIndex / 3) * 3 + (columnIndex / 3);
             int blockCellIndex = (rowIndex % 3) * 3 + columnIndex % 3;
-            indexMap[i] = columnIndex | rowIndex << 4 | blockIndex << 8 | blockCellIndex << 12;
+            columnIndexMap[i] = columnIndex;
+            rowIndexMap[i] = rowIndex;
         }
     }
 
+    static long count_getColumnIndex = 0;
+
     private static int getColumnIndex(int cellIndex) {
-        return indexMap[cellIndex] & 0xF;
+        count_getColumnIndex++;
+        return columnIndexMap[cellIndex];
     }
+
+    static long count_getRowIndex = 0;
 
     private static int getRowIndex(int cellIndex) {
-        return indexMap[cellIndex] >> 4 & 0xF;
+        count_getRowIndex++;
+        return rowIndexMap[cellIndex];
     }
+
+    static long count_getBlockIndex = 0;
 
     private static int getBlockIndex(int cellIndex) {
-        return indexMap[cellIndex] >> 8 & 0xF;
+        count_getBlockIndex++;
+        return BLOCK_INDEX[cellIndex];
     }
 
+    static long count_getBlockCellIndex = 0;
+
     private static int getBlockCellIndex(int cellIndex) {
-        return indexMap[cellIndex] >> 12 & 0xF;
+        count_getBlockCellIndex++;
+        return CELL_INDEX_IN_BLOCK[cellIndex];
     }
 
     private final int[] cells = new int[81];
@@ -70,25 +84,37 @@ class Board {
         emptyCellCount = 81;
     }
 
+    static long count_setZeroBasedNumberToCell = 0;
+
     public void setZeroBasedNumberToCell(int cellIndex, int number) {
+        count_setZeroBasedNumberToCell++;
         cells[cellIndex] = number;
+        int columnIndex = getColumnIndex(cellIndex);
+        int rowIndex = getRowIndex(cellIndex);
+        int blockIndex = getBlockIndex(cellIndex);
         if (number != -1) {
             int binaryEncodedNumber = 1 << number;
-            columns[getColumnIndex(cellIndex)] |= binaryEncodedNumber;
-            rows[getRowIndex(cellIndex)] |= binaryEncodedNumber;
-            blocks[getBlockIndex(cellIndex)] |= binaryEncodedNumber;
+            columns[columnIndex] |= binaryEncodedNumber;
+            rows[rowIndex] |= binaryEncodedNumber;
+            blocks[blockIndex] |= binaryEncodedNumber;
             emptyCellCount--;
         }
     }
 
+    static long count_clearCell = 0;
+
     public void clearCell(int cellIndex) {
+        count_clearCell++;
         emptyCellCount++;
         int number = cells[cellIndex];
         int bit = 1 << number;
         cells[cellIndex] = -1;
-        columns[getColumnIndex(cellIndex)] &= ~bit;
-        rows[getRowIndex(cellIndex)] &= ~bit;
-        blocks[getBlockIndex(cellIndex)] &= ~bit;
+        int columnIndex = getColumnIndex(cellIndex);
+        int rowIndex = getRowIndex(cellIndex);
+        int blockIndex = getBlockIndex(cellIndex);
+        columns[columnIndex] &= ~bit;
+        rows[rowIndex] &= ~bit;
+        blocks[blockIndex] &= ~bit;
     }
 
     public boolean cellIsEmpty(int cellIndex) {
@@ -99,23 +125,30 @@ class Board {
         return cells[cellIndex] == number;
     }
 
-    public int getBinaryEncodedBuddyCellsNumbers(int columnIndex, int rowIndex, int blockIndex) {
-        return columns[columnIndex] | rows[rowIndex] | blocks[blockIndex];
-    }
+    static long count_numberIsInvalidForCell = 0;
 
-    public boolean numberIsInvalidForCell(int columnIndex, int rowIndex, int blockIndex, int zeroBasedNumber) {
-        return (getBinaryEncodedBuddyCellsNumbers(columnIndex, rowIndex, blockIndex) & 1 << zeroBasedNumber) != 0;
+    public boolean numberIsInvalidForCell(int cellIndex, int zeroBasedNumber) {
+        count_numberIsInvalidForCell++;
+        int numbersSetInBuddyCells = getBinaryEncodedSetNumbersInBuddyCells(cellIndex);
+        return (numbersSetInBuddyCells & 1 << zeroBasedNumber) != 0;
     }
 
     public String asString() {
         return Arrays.stream(cells).mapToObj(n -> String.valueOf(n + 1)).collect(Collectors.joining());
     }
 
+    static long count_getBinaryEncodedCandidates = 0;
+
     public int getBinaryEncodedCandidates(int cellIndex) {
-        int columnIndex = getColumnIndex(cellIndex);
-        int rowIndex = getRowIndex(cellIndex);
-        int blockIndex = getBlockIndex(cellIndex);
-        int binaryEncodedSetNumbers = columns[columnIndex] | rows[rowIndex] | blocks[blockIndex];
-        return binaryEncodedSetNumbers ^ 0x1FF;
+        count_getBinaryEncodedCandidates++;
+        return getBinaryEncodedSetNumbersInBuddyCells(cellIndex) ^ 0x1FF;
+    }
+
+    static long count_getBinaryEncodedSetNumbersInBuddyCells = 0;
+    static long count_getBinaryEncodedSetNumbersInBuddyCells_cacheHit = 0;
+
+    private int getBinaryEncodedSetNumbersInBuddyCells(int cellIndex) {
+        count_getBinaryEncodedSetNumbersInBuddyCells++;
+        return columns[getColumnIndex(cellIndex)] | rows[getRowIndex(cellIndex)] | blocks[getBlockIndex(cellIndex)];
     }
 }
